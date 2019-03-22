e18093827ba042054b714e470de0cf19689c50e5
Kris Coppieters <kris@rorohiko.com>
2019-03-23 10:49:09 +1300

    SETUP.jsx is a snippet with code which configures various modules based on the configuration data

---
+++ StyleCleanup/Source/SETUP.jsx
@@ -0,0 +1,6 @@
+// Pass configuration data to LOG module
+
+LOG.LOG_TO_CONSOLE   = LOG_TO_CONSOLE;
+LOG.LOG_LEVEL        = LOG_LEVEL;
+LOG.LOG_ENTRY_EXIT   = LOG_ENTRY_EXIT;
+LOG.FILE_PATH_LOG    = FILE_PATH_LOG;
